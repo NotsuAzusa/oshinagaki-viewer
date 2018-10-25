8d4eca47a0494bec19276754b05af5731ed54856
野津あずさ <azusa-notsu@notsuazusanoMac-mini.local>
2018-10-25 22:04:03 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+'use strict';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Electronのモジュール
+const electron = require("electron");
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// アプリケーションをコントロールするモジュール
+const app = electron.app;
+
+// ウィンドウを作成するモジュール
+const BrowserWindow = electron.BrowserWindow;
+
+// メインウィンドウはGCされないようにグローバル宣言
+let mainWindow;
+
+// 全てのウィンドウが閉じたら終了
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+// Electronの初期化完了後に実行
+app.on('ready', function() {
+  // メイン画面の表示。ウィンドウの幅、高さを指定できる
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  // ウィンドウが閉じられたらアプリも終了
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
